9358e90ddf6da156b90ca481aebf841583d6a974
Jose Arce y de la Borbolla <joseab10@gmail.com>
2024-02-20 12:48:01 +0100

    Fixed bug again, this time for good I think.

--- setup.sh
+++ setup.sh
@@ -4,13 +4,13 @@
 SETTINGS_DIR=~/etc
 SCRIPT_DIR=$(dirname "$0")
 
-echo Creating $SETTINGS_DIR
+echo "Creating $SETTINGS_DIR"
 mkdir $SETTINGS_DIR
 
-echo Installing files to $SETTINGS_DIR
+echo "Installing files to $SETTINGS_DIR"
 cp -r "$SCRIPT_DIR" "$SETTINGS_DIR"
 
-echo Adding source commands to ~/.bashrc
+echo "Adding source commands to ~/.bashrc"
 cd "$SCRIPT_DIR"
 cat etc/bashrc >> ~/.bashrc
 
@@ -18,7 +18,8 @@ if hash conda 2>/dev/null; then
 	echo "Setting Anaconda to not modify the shell's prompt"
 	conda config --set changeps1 False
 else
-	echo Anaconda not found. Ignoring conda settings.
+	echo "Anaconda not found. Ignoring conda settings."
 fi
 
+echo "Sourcing .bashrc profile to take effect."
 source ~/.bashrc
